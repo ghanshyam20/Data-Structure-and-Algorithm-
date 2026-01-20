958ca9305f85ff4d249e58c4e07bfffa9acc044e
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2026-01-20 15:05:18 +0200

    update

---
+++ chapter1/eight.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def hello(self):
+        message = f"Hello, my name is {self.name}"
+        print(message)
+        return message
+
+
+
