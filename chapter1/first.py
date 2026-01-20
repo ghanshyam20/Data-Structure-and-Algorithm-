90a68216598c486fd54661b18684711d5f93b704
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2026-01-20 13:56:41 +0200

    excercise 1 pushed

---
+++ chapter1/first.py
@@ -0,0 +1,12 @@
+s = input()
+print("".join(sorted(s)))
+
+
+
+# input() = this will take and reads the string
+
+# sorted(s) = this will sorts characters by Unicode
+
+# "".join(...) = this iwlll joins list back into a string
+
+# print() = this will print
