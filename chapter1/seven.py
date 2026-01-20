45b7e0dcd520168c465d0e3de46aa756dce9ea23
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2026-01-20 14:58:26 +0200

    update code

--- chapter1/seven.py
+++ chapter1/seven.py
@@ -12,4 +12,9 @@ def custom_encoder(text):
     return result
 
 
+text = input("enter string ")   # i have add this to see here in terminal so slighlyt modification of code than in moodle.hope you will excuse me ..
+print(custom_encoder(text)) 
+
+
+
 
